304c24c1fd490087e5ec46bd9ae641b7ccaff250
Chaithanya S Poojary <chaitanyaspoojary17@gmail.com>
2025-09-13 22:17:09 +0530

    Delete multiplication_table/csp_multiplication.py

--- multiplication_table/csp_multiplication.py
+++
@@ -1,7 +0,0 @@
-"""Multiplication Table Generator
-Input a number and print its multiplication table up to 10."""
-
-n = int (input("Enter the number:"))
-
-for i in range(1,11):
-    print(n ,"x", i ,"=", n*i)
